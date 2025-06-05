124054c8adbc2c2817e16909e28a36fb2b1dcc00
Laurent Meert <laurent_meert@yahoo.com>
2025-06-05 21:26:20 +0200

    Initial Commit

---
+++ github-viewer/github_viewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_viewerApp.swift
+//  github-viewer
+//
+//  Created by Laurent Meert on 05/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct github_viewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ github-viewerTests/github_viewerTests.swift
@@ -0,0 +1,16 @@
+//
+//  github_viewerTests.swift
+//  github-viewerTests
+//
+//  Created by Laurent Meert on 05/06/2025.
+//
+
+import Testing
+
+struct github_viewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
